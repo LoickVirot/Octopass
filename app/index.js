91c2e67e7d16aece051715322395d2bad23c768a
Loïck Virot <virot.loick@gmail.com>
2018-03-26 20:01:35 +0200

    Contol headers

--- app/index.js
+++ app/index.js
@@ -1,12 +1,21 @@
 const server = require('server')
 const { get, post } = server.router
+const { header } = server.reply
 const mongoose = require('mongoose')
 
 mongoose.connect(require('./config/database'), { useMongoClient: true })
 mongoose.Promise = global.Promise
 
+const cors = [
+  ctx => header("Access-Control-Allow-Origin", "*"),
+  ctx => header('Access-Control-Allow-Methods, "*'),
+  ctx => header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"),
+  ctx => ctx.method.toLowerCase() === 'options' ? 200 : false
+];
+
 server(
   require('./config/server'),
+  cors,
   ctx => {
     ctx.mongoose = mongoose
   },
